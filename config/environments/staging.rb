a7a8727fbc16396e84d624d0b06f72ce06314c1a
Ryan Epp <hey@ryanepp.com>
2018-06-14 18:06:06 -0400

    temporarily allowing errors to show in staging for faster debugging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
   config.serve_static_files = true
   config.assets.js_compressor = :uglifier
